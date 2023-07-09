94926e6da814e9892cb289c76947ee38cd18cb7b
hjazz <hjsung@gmail.com>
2023-07-09 10:59:14 +0900

    initial commit

---
+++ MyFirst/ByeView.swift
@@ -0,0 +1,20 @@
+//
+//  ByeView.swift
+//  MyFirst
+//
+//  Created by hjazz on 2023/07/09.
+//
+
+import SwiftUI
+
+struct ByeView: View {
+    var body: some View {
+        Text("Bye bye, World!")
+    }
+}
+
+struct ByeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ByeView()
+    }
+}
